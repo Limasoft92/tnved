bba60f83df0dd0beffa40889eb23d172e17a8ce5
Limasoft92 <limasoft.unity@gmail.com>
2016-12-30 14:00:05 +0300

    надо разобраться с регулярными выражениями

--- TNVED/Form1.Designer.cs
+++ TNVED/Form1.Designer.cs
@@ -32,13 +32,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.codesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.codesBindingSource)).BeginInit();
@@ -47,83 +45,69 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 56);
+            this.label1.Location = new System.Drawing.Point(665, 664);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Введите код ТН ВЭД:";
+            this.label1.Size = new System.Drawing.Size(161, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Стоимость с учётом пошлины:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(43, 105);
+            this.label2.Location = new System.Drawing.Point(12, 610);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.Size = new System.Drawing.Size(62, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Ваши коды:";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(406, 56);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 37);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Добавить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label2.Text = "Результат:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(46, 72);
+            this.textBox1.Location = new System.Drawing.Point(12, 626);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(636, 92);
             this.textBox1.TabIndex = 1;
-            // 
-            // listBox1
-            // 
-            this.listBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.listBox1.Location = new System.Drawing.Point(46, 121);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(468, 56);
-            this.listBox1.TabIndex = 4;
-            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(188, 72);
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(668, 626);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(328, 19);
             this.textBox2.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(188, 56);
+            this.label3.Location = new System.Drawing.Point(665, 610);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.Size = new System.Drawing.Size(133, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Введите стоимость:";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(328, 56);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 37);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Изменить";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Visible = false;
+            this.label3.Text = "Общая стоимость груза:";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 187);
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 98);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(760, 363);
-            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.Size = new System.Drawing.Size(984, 502);
+            this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridView1_DataError);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(668, 680);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(328, 31);
+            this.textBox3.TabIndex = 8;
             // 
             // codesBindingSource
             // 
@@ -133,14 +117,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -159,14 +141,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.BindingSource codesBindingSource;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- TNVED/Form1.cs
+++ TNVED/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace TNVED
 {
@@ -18,6 +20,9 @@ namespace TNVED
         int z = 0;
         List<Codes> code2 = new List<Codes>();
         BindingSource source = new BindingSource();
+        double taxSum = 0, sum = 0, taxCost = 0, taxCostNds = 0, taxCostSum = 0, taxCostNdsSum = 0;
+        double nds = (double)0.18;
+        double tax1 = 0;
 
         public Form1()
         {
@@ -25,71 +30,34 @@ namespace TNVED
             code1 = ob.WriteInRAM();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string num = textBox1.Text;
-            string cos = textBox2.Text;
-
-            if (num == "")
-            {
-                MessageBox.Show("Вы не ввели код ТН ВЭД!");
-            }
-            else if (num.Length != 10)
-            {
-                MessageBox.Show("Введите 10-значный код ТН ВЭД!");
-            }
-            else if (cos == "")
-            {
-                MessageBox.Show("Вы не ввели стоимость!");
-            }
-            else
-            {
-                foreach (Codes c1 in code1)
-                {
-                    if (c1.number.Contains(num))
-                    {
-                        c1.cost = (float)Convert.ToDouble(cos);
-                        listBox1.Items.Add(c1.number + "     " + c1.description + "     " + c1.unit + "     " + c1.tax + "     " + c1.cost);
-                        textBox1.Text = "";
-                        textBox2.Text = "";
-                    }
-                    else
-                    {
-                        z++;
-                    }
-                }
-                if (z == code1.Count)
-                {
-                    MessageBox.Show("Код " + num + " отсутствует в базе данных!");
-                    z = 0;
-                }
-            }
-        }
-        
-        private void listBox1_Click(object sender, EventArgs e)
-        {
-            if (listBox1.SelectedIndex >= 0)
-            {
-                button2.Visible = true;
-            }
-            else
-            {
-                button2.Visible = false;
-            }
-        }
-
         private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
             int ind = 0;
             bool yes = false;
-            //string io = dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString();
-            int ec = e.ColumnIndex;
-            int er = e.RowIndex;
+            int eCol = e.ColumnIndex;
+            int eRow = e.RowIndex;
+            string curCell = "";
+            char[] symbol = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            
+            try                                            //проверка на ввод букв
+            {
+                long s = Convert.ToInt64(dataGridView1[eCol, eRow].Value);
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Ввод букв не допускается! Пожалуйста, исправьте!");  
+            }
+
+            if (dataGridView1[eCol, eRow].Value != null)
+            {
+                curCell = dataGridView1[eCol, eRow].Value.ToString();
+            }
+
             foreach (Codes c1 in code1)
             {
-                if (dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString() != null)
+                if (curCell != null)                        //если текущая ячейка не равна нуля (т.е. изменена)
                 {
-                    if (c1.number.Equals(dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString()))
+                    if (c1.number.Equals(curCell))          //если код содержится в файле
                     {
                         yes = true;
                         ind = c1.id;
@@ -97,134 +65,205 @@ namespace TNVED
                     }
                 }
             }
+
             if (yes) {
-                code2[e.RowIndex] = code1[ind];
-                code2[e.RowIndex].id = e.RowIndex + 1;
-                code2.Add(new Codes());
-                source.ResetBindings(false);
-                dataGridView1[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.Green;
+                if (code2[eRow].id == 0)
+                {
+                    code2[eRow].Copy(code1[ind]);
+                    code2[eRow].id = code2.Count;                  
+                    code2.Add(new Codes());
+                }
+                else
+                {
+                    code2[eRow].Copy(code1[ind]);
+                }
+                source.ResetBindings(false);         
+                if (eCol == 1)                    //подсветка только столбца "Код ТН ВЭД"
+                {
+                    dataGridView1[eCol, eRow].Style.BackColor = Color.LightGreen;
+                }
+
+                dataGridView1.CurrentCell = dataGridView1[eCol, eRow + 1];       //перевод курсора на следующуу строку
             }
             else
             {
-                dataGridView1[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.Red;
+                if (eCol == 1)                    //подсветка только столбца "Код ТН ВЭД"
+                {
+                    dataGridView1[eCol, eRow].Style.BackColor = Color.Tomato;
+                }
             }
+
+            sum = 0;
+            taxSum = 0;
+            taxCostSum = 0;
+            taxCostNdsSum = 0;
+
+            string pattern = @"";
+            Regex regex = new Regex(pattern);
+
+            foreach (Codes c2 in code2)
+            {
+                if (c2.tax.Contains("не менее"))
+                {
+                    Match match = regex.Match(c2.tax);
+                    tax1 = match.Groups[1].Value.Substring;
+                    /*int temp = 0;
+                    int temp2 = 0;
+                    char[] array = c2.tax.ToCharArray();
+                    for (int j = 0; j < array.Length; j++)      //поиск первой цифры
+                    {
+                        //temp = c2.tax.IndexOf(symbol[j]);
+                        if (char.IsDigit(array[j]))
+                        {
+                            temp = j;
+                        }
+                    }
+                    for (int k = 0; k < c2.tax.Length; k++)
+                    {
+                        temp2 = c2.tax.IndexOf(" ", temp);
+                    }
+                    tax1 = double.Parse(c2.tax.Substring(temp, (temp2 - temp)));*/
+                }
+                else
+                {
+                    sum += c2.cost;                                         //общая стоимость груза без пошлин
+                    taxCost = c2.cost * double.Parse(c2.tax) / 100;         //пошлина по коду
+                    taxCostSum += taxCost;                                  //сумма пошлины
+                    taxCostNds = (c2.cost + taxCost) * nds;                 //НДС по коду
+                    taxCostNdsSum += taxCostNds;                            //сумма НДС
+                    taxSum += c2.cost + taxCost + taxCostNds;               //общая стоимость с учётом всех пошлин
+                }
+            }
+            textBox1.Text = "Стоимость груза без учёта пошлин составляет " + Convert.ToString(Math.Round(sum, 4)) + " €\r\n" +
+                "Сумма пошлины составляет " + Convert.ToString(Math.Round(taxCostSum, 4)) + " €\r\n" +
+                   "Сумма НДС составляет " + Convert.ToString(Math.Round(taxCostNdsSum, 4)) + " €\r\n" +
+                       "Стоимость груза с учётом всех пошлин составляет " + Convert.ToString(Math.Round(taxSum, 4)) + " €" + tax1;
+            textBox2.Text = Convert.ToString(Math.Round(sum,2)) + " €";
+            textBox3.Text = Convert.ToString(Math.Round(taxSum,2)) + " €";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            source.DataSource = code1;
+            source.DataSource = code2;
             dataGridView1.DataSource = source;
             code2.Add(new Codes());
             source.ResetBindings(false);
             dataGridView1.AllowUserToAddRows = false;
-            dataGridView1.Columns[0].ReadOnly = true;
-            dataGridView1.Columns[2].ReadOnly = true;
-            dataGridView1.Columns[3].ReadOnly = true;
-            dataGridView1.Columns[4].ReadOnly = true;
-            dataGridView1.Columns[0].Width = 40;
-            dataGridView1.Columns[1].Width = 100;
-            dataGridView1.Columns[2].Width = 316;
-            dataGridView1.Columns[3].Width = 60;
-            dataGridView1.Columns[4].Width = 100;
-            dataGridView1.Columns[5].Width = 100;
 
-            
+            dataGridView1.Columns[0].ReadOnly = true;
+            dataGridView1.Columns[0].Width = 40;
+            dataGridView1.Columns[0].DefaultCellStyle.BackColor = Color.Gainsboro;
+
+            dataGridView1.Columns[1].Width = 100;
+            ((DataGridViewTextBoxColumn)dataGridView1.Columns[1]).MaxInputLength = 10;
+
+            dataGridView1.Columns[2].ReadOnly = true;
+            dataGridView1.Columns[2].Width = 320;
+            dataGridView1.Columns[2].DefaultCellStyle.BackColor = Color.Gainsboro;
+
+            dataGridView1.Columns[3].ReadOnly = true;
+            dataGridView1.Columns[3].Width = 60;
+            dataGridView1.Columns[3].DefaultCellStyle.BackColor = Color.Gainsboro;
+
+            dataGridView1.Columns[4].ReadOnly = true;
+            dataGridView1.Columns[4].Width = 320;
+            dataGridView1.Columns[4].DefaultCellStyle.BackColor = Color.Gainsboro;
+
+            dataGridView1.Columns[5].Width = 100;
+            ((DataGridViewTextBoxColumn)dataGridView1.Columns[5]).MaxInputLength = 15;
+        }
+
+        private void dataGridView1_DataError(object sender, DataGridViewDataErrorEventArgs e)           //для исключения ошибки при вводе пустого поля
+        {
+            if (dataGridView1[e.ColumnIndex, e.RowIndex].Value == null)
+            {
+                dataGridView1[e.ColumnIndex, e.RowIndex].Value = (double)0;
+            }
         }
     }
 
 
     public class ReadFromFile
     {
-        public  List<Codes> WriteInRAM()
+        public List<Codes> WriteInRAM()
         {
             string[] lines = System.IO.File.ReadAllLines(@"D:\Development\CODES\codes_all.txt");
-            string temp = "", temp2 = "", temp3 = "", temp4 = "", temp5 = "";
+            string temp = "", temp2 = "", temp3 = "", temp4 = "";
             char[] symbol = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
             char[] array;
-            int first=0, last=0, i = 0;
-            int isLet = 0;
+            char[] probel = {' '};
+            int i = 0;
+            int isLet = 0, isDig = 0;
+
+            StreamWriter sw = new StreamWriter(@"D:\Development\CODES\log.txt",false);
 
             List<Codes> code = new List<Codes>();
             Codes obj2 = new Codes();
 
             foreach (string line in lines)
             {
-                if (line.Length > 70)
+                if (line.Length > 74)
                 {
                     Codes obj = new Codes();
-                    if (isLet == 3)
+                    isDig = 0;
+                    temp = line.Substring(0, 15);
+                    temp = temp.Replace(" ", string.Empty);
+                    temp = temp.Trim('+');
+                    temp2 = temp;
+                    temp3 = temp;
+                    temp4 = temp;
+                    array = temp.ToCharArray();
+                    for (int j = 0; j < array.Length; j++)      //количество цифр в строке
                     {
-                        first = 0;
-                        last = 0;
-                        temp = line.Remove(5, 1);
-                        temp = temp.Remove(7, 1);
-                        temp = temp.Remove(10, 1);
-                        temp2 = temp;
-                        temp3 = temp;
-                        temp4 = temp;
-                        temp = temp.Substring(1, 10);
-                        first = temp.IndexOfAny(symbol);
-                        last = temp.LastIndexOfAny(symbol);
-                        if ((last - first) != 9)                        
+                        if (char.IsDigit(array[j]))
                         {
-                            obj2.tax = temp5 + line.Substring(56, 17);
-                            code.Add(obj2);
+                            isDig++;
+                        }
+                    }
+                    if (isDig != 10 && isLet >= 2 && isLet <= 3)               
+                    {
+                        obj2.tax += line.Substring(57, 18);
+                        obj2.tax = Regex.Replace(obj2.tax, @"\s+", " ");
+                        isLet++;
+                        //sw.WriteLine(line);
+                        continue;
+                    }
+                    if (isDig == 10 && isLet >= 2)               
+                    {
+                        code.Add(obj2);
+                        sw.WriteLine(obj2.tax);
+                        isLet = 0;
+                        i++;
+                    }
+                    if (isDig == 10)                                //если обнаружен 10-значный код
+                    {
+                        isLet = 0;
+                        obj.id = i;
+                        obj.number = temp;
+                        temp2 = line.Substring(16, 32);
+                        obj.description = temp2;
+                        temp3 = line.Substring(49, 6);
+                        obj.unit = temp3;
+                        temp4 = line.Substring(57, 18);
+                        array = temp4.ToCharArray();
+                        for (int j = 0; j < array.Length; j++)      //проверка на наличие букв в строке с пошлиной
+                        {
+                            if (char.IsLetter(array[j]))
+                            {
+                                isLet++;
+                            }
+                            if (isLet == 2)
+                            {
+                                break;
+                            }
+                        }
+                        obj.tax = temp4;                            //если поле "Пошлина" без букв
+                        obj2 = obj;
+                        if (isLet <= 1)
+                        {
+                            code.Add(obj);
                             i++;
-                            isLet = 0;
                         }
-                        else
-                        {
-                            isLet = 0;
-                        }
-                    }
-                    if (isLet == 0)
-                    {
-                        first = 0;
-                        last = 0;
-                        temp = line.Remove(5, 1);
-                        temp = temp.Remove(7, 1);
-                        temp = temp.Remove(10, 1);
-                        temp2 = temp;
-                        temp3 = temp;
-                        temp4 = temp;
-                        temp = temp.Substring(1, 10);
-                        first = temp.IndexOfAny(symbol);
-                        last = temp.LastIndexOfAny(symbol);
-                        if ((last - first) == 9)                    //если обнаружен 10-значный код
-                        {
-                            obj.id = i;
-                            obj.number = temp;
-                            temp2 = temp2.Substring(15, 31);
-                            obj.description = temp2;
-                            temp3 = temp3.Substring(49, 6);
-                            obj.unit = temp3;
-                            temp4 = temp4.Substring(56, 16);
-                            array = temp4.ToCharArray();
-                            for (int j = 0; j < array.Length; j++)      //проверка на наличие букв в строке с пошлиной
-                            {
-                                if (char.IsLetter(array[j]))
-                                {
-                                    isLet = 1;
-                                    break;
-                                }
-                            }
-                            obj.tax = temp4;
-                            obj2 = obj;
-                            if (isLet == 0)
-                            {
-                                code.Add(obj);
-                                i++;
-                            }
-                        } 
-                    }
-                    if (isLet == 2)
-                    {
-                        temp5 = temp4 + line.Substring(56, 18);
-                        isLet = 3;
-                    }
-                    if (isLet == 1)     //пропуск строки
-                    {
-                        isLet = 2;
                     }
                 }
             }
@@ -246,7 +285,7 @@ namespace TNVED
         [System.ComponentModel.DisplayName("Пошлина, %")]
         public string tax {get; set;}
         [System.ComponentModel.DisplayName("Стоимость")]
-        public float cost {get; set;}
+        public double cost {get; set;}
 
         public Codes() 
         {
@@ -254,8 +293,16 @@ namespace TNVED
             number = "";
             description = "";
             unit = "";
-            tax = "";
+            tax = "0";
             cost = 0;
+        }
+
+        public void Copy (Codes o)
+        {
+            number = o.number;
+            description = o.description;
+            unit = o.unit;
+            tax = o.tax;
         }
     }
 }
